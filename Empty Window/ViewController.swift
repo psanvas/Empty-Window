f4ca5b8f9cee487e47a5b81ce3cbaba7c312a9c5
Phillip Sanchez <psanvas@gmail.com>
2021-05-31 11:40:35 -0600

    Initial Commit

---
+++ Empty Window/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Empty Window
+//
+//  Created by Phillip Sanchez-Vasquez on 5/31/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
