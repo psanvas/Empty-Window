89861780900bfb8b3e5ca20c3b6899294929f783
Phillip Sanchez <psanvas@gmail.com>
2021-05-31 13:06:33 -0600

    Test

--- Empty Window/ViewController.swift
+++ Empty Window/ViewController.swift
@@ -12,6 +12,9 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        let allViews = Bundle.main.loadNibNamed("View", owner: nil)!
+        let onlyView = allViews[0] as! UIView
+        self.view.addSubview(onlyView)
     }
 
 
